2ab593c135e38331ef80f08008bc249c001025be
Administrator <Administrator@WIN-42IT1SV43H7>
2020-08-30 06:53:51 -0700

    Van Loi

---
+++ WPF_TimeSync/SQLConnectAbstract/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLConnectAbstract
+{
+    public class Class1
+    {
+    }
+}
