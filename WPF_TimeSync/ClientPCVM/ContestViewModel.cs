c49db71056dc68381a7faeb6492b260b8d4e2591
Administrator <Administrator@WIN-42IT1SV43H7>
2020-09-03 19:39:32 -0700

    Add Clinet View

---
+++ WPF_TimeSync/ClientPCVM/ContestViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientPCVM
+{
+    public class ContestViewModel : BaseViewModel
+    {
+        TFQuesViewModel _trueFalseQues;
+        public TFQuesViewModel TrueFalseQuestion
+        { 
+            get => _trueFalseQues;
+            set
+            {
+                _trueFalseQues = value;
+                OnPropertyChanged("TrueFalseQuestion");
+            }
+        }
+
+        public ContestViewModel()
+        {
+            TrueFalseQuestion = new TFQuesViewModel();
+        }
+    }
+}
